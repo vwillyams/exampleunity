cc642af1643203ccdcdffa77c91520ba9a6e9ea6
Victor Willyams <victorw@unity3d.com>
2019-05-17 13:24:36 -0500

    basic settings which may be required for android build failure

---
+++ Assets/Editor/BuildPlayer.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+namespace Editor
+{
+
+// Output the build size or a failure depending on BuildPlayer.
+
+    public class BuildPlayer : MonoBehaviour
+    {
+        [MenuItem("Build/Build Android")]
+        public static void MyBuild()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.locationPathName = "androidBuild";
+            buildPlayerOptions.target = BuildTarget.Android;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+    }
+}
+
+#endif
