45e49896790d8ee04ef7cc2aec69e9319b263b52
Victor Willyams <victorw@unity3d.com>
2019-05-17 13:17:36 -0500

    fix BuildPlayer script

--- Assets/Editor/BuildPlayer.cs
+++ Assets/Editor/BuildPlayer.cs
@@ -4,29 +4,33 @@ using UnityEditor;
 using UnityEngine;
 using UnityEditor.Build.Reporting;
 
+namespace Editor
+{
+
 // Output the build size or a failure depending on BuildPlayer.
 
-public class BuildPlayer : MonoBehaviour
-{
-    [MenuItem("Build/Build Android")]
-    public static void MyBuild()
+    public class BuildPlayer : MonoBehaviour
     {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.locationPathName = "androidBuild";
-        buildPlayerOptions.target = BuildTarget.Android;
-        buildPlayerOptions.options = BuildOptions.None;
-
-        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        BuildSummary summary = report.summary;
-
-        if (summary.result == BuildResult.Succeeded)
+        [MenuItem("Build/Build Android")]
+        public static void MyBuild()
         {
-            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-        }
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.locationPathName = "androidBuild";
+            buildPlayerOptions.target = BuildTarget.Android;
+            buildPlayerOptions.options = BuildOptions.None;
 
-        if (summary.result == BuildResult.Failed)
-        {
-            Debug.Log("Build failed");
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
         }
     }
 }
